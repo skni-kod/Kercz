37ba81b340870789f48a308883e8454d52c15bfc
Konrad Bochenek <56956303+Kazan1520@users.noreply.github.com>
2022-10-17 22:30:35 +0200

    Update settings.py

--- projektio/settings.py
+++ projektio/settings.py
@@ -11,11 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -89,7 +87,7 @@ WSGI_APPLICATION = 'projektio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -137,4 +135,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafolder") 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfolder")
 MEDIA_URL = '/files/'
-django_heroku.settings(locals())
