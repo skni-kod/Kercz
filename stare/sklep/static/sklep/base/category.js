7fd635ae0959bda8169d96892799b65ec6f12de0
Carola321 <102753696+Carola321@users.noreply.github.com>
2022-11-12 19:00:18 +0100

    Fixed new project

--- stare/sklep/static/sklep/base/category.js
+++
@@ -1,6 +0,0 @@
-let button = document.getElementById('button')
-let div = document.getElementById('sorted_fun')
-
-button.addEventListener('click',function(){
-    div.classList.contains('hide') ? div.classList.remove('hide') : div.classList.add('hide')
-})
--- stare/sklep/static/sklep/order/address_selection.js
+++
@@ -1,7 +0,0 @@
-const adresSelecion = document.getElementById('adres-selection')
-let div = document.getElementById('new-address')
-console.log(adresSelecion.length)
-
-adresSelecion.addEventListener('change',function(){
-    this.value == 'new-adress' ? div.classList.remove('hide') : div.classList.add('hide')
-})
