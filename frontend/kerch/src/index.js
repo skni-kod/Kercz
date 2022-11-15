744dcbac679937488931b160dc2b9158fb50397b
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2022-11-15 16:32:32 +0100

    Added router package

--- frontend/kerch/src/index.js
+++ frontend/kerch/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './views/App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './views/app';
+import Home from './views/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path='/home' element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
---
+++ frontend/kerch/src/views/app.jsx
@@ -0,0 +1,24 @@
+import '../assets/css/App.css';
+import { Outlet, Link } from "react-router-dom";
+App = () => {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+export default App;
