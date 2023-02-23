e318cb8b45440cea88ce2efd2b16294fc0aaa0b4
Skalik2 <106670239+Skalik2@users.noreply.github.com>
2023-02-23 14:01:02 +0100

    Shopping-cart / UI update

--- frontend/kerch/src/components/itemCard.jsx
+++ frontend/kerch/src/components/itemCard.jsx
@@ -1,16 +1,39 @@
+import "../assets/css/components/itemCard.css";
 import React from 'react'
 
 const ItemCard = ({ item, currency}) => {
-    const { img , name, size, price, quantity} = item;
+    const { img , name, size, price, quantity, brand} = item;
   return (
-    <tr>
-        <td className="product-field"><img src={img} alt={name} /></td>
-        <td><p>{name}</p></td>
-        <td><p>{size}</p></td>
-        <td><p>{price} {currency}</p></td>
-        <td><p>{quantity}</p></td>
-        <td><b><p>{quantity * price} {currency}</p></b></td>
-    </tr>
+    <div className="item-card">
+      <div className="flex-center">
+        <div className="img-div-card">
+          <td className="product-field"><img src={img} alt={name} /></td>
+        </div>
+        <div className="description-div-card">
+          <p className="item-price-card">{price}{currency}</p>
+          <p className="item-brand-card">{brand}</p>
+          <p className="item-name-card">{name}</p>
+          <hr></hr>
+          <div className="flex-row">
+            <p className="item-size-card">Rozmiar</p>
+            <select name="sizeSelect" value={size}> {/* wartosci pobierane z bazy (dostępne rozmiary dla danego produktu) */}
+              <option value="39">39</option>
+              <option value="40">40</option>
+              <option value="41">41</option>
+              <option value="42">42</option> 
+            </select>
+            <p className="item-quantity-card">Ilość</p>
+            <select name="quantitySelect" value={quantity}> {/* wartosci pobierane z bazy (ilosc danego produktu)*/}
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option> 
+            </select>
+          </div>
+          <hr></hr>
+        </div>
+      </div>
+      <hr></hr>
+    </div>
   )
 }
 
--- frontend/kerch/src/views/shoppingCart.jsx
+++ frontend/kerch/src/views/shoppingCart.jsx
@@ -1,5 +1,13 @@
-import "../assets/css/components/itemCard.css";
+import '../assets/css/views/shoppingCart.css';
 import { ItemCard } from "../components";
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import Delivery1 from '../assets/icons/delivery1.svg';
+import Delivery2 from '../assets/icons/delivery2.svg';
+import Delivery3 from '../assets/icons/delivery3.svg';
+import Payment1 from '../assets/icons/payment1.svg';
+import Payment2 from '../assets/icons/payment2.png';
+import KerczLogo from '../assets/icons/kerczLogo.svg';
 
 const ShoppingCart = () =>{
     // TO DO:Future list is passed from other source
@@ -8,6 +16,7 @@ const ShoppingCart = () =>{
             id: 123456789,
             img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/fdd2ed4315b74f6ea506acb600b20504_9366/OZWEEGO_Shoes_Bezowy_FX6029_01_standard.jpg',
             name: 'OZWEEGO',
+            brand: 'adidas',
             size: 42,
             price: 549,
             quantity: 1,
@@ -16,6 +25,7 @@ const ShoppingCart = () =>{
             id: 123456788,
             img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/6d8f07398f114ae897b0adfd010611de_9366/ZX_1K_Boost_2.0_Shoes_Czern_GZ3551_01_standard.jpg',
             name: 'ZX 1K BOOST 2.0',
+            brand: 'adidas',
             size: 39,
             price: 499,
             quantity: 2,
@@ -24,6 +34,7 @@ const ShoppingCart = () =>{
             id: 123456787,
             img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3bbecbdf584e40398446a8bf0117cf62_9366/Samba_OG_Shoes_Bialy_B75806_01_standard.jpg',
             name: 'SAMBA OG SHOES',
+            brand: 'adidas',
             price: 549,
             size: 40,
             quantity: 1,
@@ -34,39 +45,165 @@ const ShoppingCart = () =>{
     exampleList.forEach(element => {
         total += (element.price * element.quantity);
     });
+    function currentYear() {
+        const dateNow = new Date();
+        return dateNow.getFullYear();
+    }
+
+    const [delivery, setDelivery] = useState("0,00");
     return (
-        <div className="main-shopping-cart">
-            <h2>Shopping Cart</h2>
-            <div className="main-shopping-cart-table">
-                <table>
-                    <thead>
+    <div className='main-shopping-cart'>
+        <div className='shopping-baner'>
+            <img className='kercz-baner-img' src={KerczLogo}></img>
+            <p className="first"><b>1. Zawartość</b></p>
+            <hr/>
+            <p className='second'>2. Dane adresowe</p>
+            <hr/>
+            <p className='third'>3. Potwierdzenie</p>
+        </div>
+        <hr/>
+        <h2 className='main-name-cart'>zawartość</h2>
+        <div className='inner-main-shopping-cart'>
+            <div className='left-shopping-cart'>
+                
+                <hr></hr>
+                {
+                    exampleList.map((item)=>(
+                        <ItemCard key={item.id} item={item} currency={currency} />
+                    ))
+                }
+                <h2>metoda dostawy</h2>
+                <div className='delivery-container'>
+                    <div className='column-flex'>
+                        <label for="delivery1" onClick={()=>setDelivery("8.99")}>
+                            <div className='row-flex delivery-margin'>
+                                <input type="radio" id="delivery1" name="delivery-choose"/>
+                                <img className='delivery-img' src={Delivery1}></img>
+                                <p>Kurier InPost</p>
+                                <div className='delivery-right-allign'>
+                                    <p className='delivery-price'>8,99 zł</p>
+                                </div>
+                            </div>
+                        </label>
+                        <hr/>
+                        <label for="delivery2" onClick={()=>setDelivery("8.99")}>
+                            <div className='row-flex delivery-margin'>
+                                <input type="radio" id="delivery2" name="delivery-choose" />
+                                <img className='delivery-img' src={Delivery2}></img>
+                                <p>InPost Paczkomat 24/7</p>
+                                <div className='delivery-right-allign'>
+                                    <p className='delivery-price'>8,99 zł</p>
+                                </div>
+                            </div>
+                        </label>
+                        <hr/>
+                        <label for="delivery3" onClick={()=>setDelivery("0,00")}>
+                            <div className='row-flex delivery-margin'>
+                                <input type="radio" id="delivery3" name="delivery-choose"/>
+                                <img className='delivery-img' src={Delivery3}></img>
+                                <p>Odbiór w sklepie</p>
+                                <div className='delivery-right-allign'>
+                                    <p className='delivery-price'>0,00 zł</p>
+                                </div>
+                            </div>
+                        </label>
+                        <hr/>
+                    </div>
+                </div>
+                <h2>metoda płatności</h2>
+                <div className='payment-container'>
+                    <div className='column-flex'>
+                        <label for="payment1">
+                            <div className='row-flex payment-margin'>
+                                <input type="radio" id="payment1" name="payment-choose"/>
+                                <img className='payment-img' src={Payment1}></img>
+                                <p>Blik</p>
+                            </div>
+                        </label>
+                        <hr/>
+                        <label for="payment2">
+                            <div className='row-flex payment-margin'>
+                                <input type="radio" id="payment2" name="payment-choose"/>
+                                <img className='payment-img' src={Payment2}></img>
+                                <p>Szybki przelew</p>
+                            </div>
+                        </label>
+                        <hr/>
+                        <label for="payment3">
+                            <div className='row-flex payment-margin'>
+                                <input type="radio" id="payment3" name="payment-choose"/>
+                                <img className='payment-img monochrome' src={Delivery3}></img>
+                                <p>Płatność przy odbiorze</p>
+                            </div>
+                        </label>
+                        <hr/>
+                    </div>
+                </div>
+            </div>
+            
+            <div className='right-shopping-cart'>
+                <hr></hr>
+                <div className='right-inner-shopping-cart'>
+                    
+                    <h3>Podsumowanie</h3>
+                    <table>
                         <tr>
-                            <th className="product-field"></th>
-                            <th>Product</th>
-                            <th>Size</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Total</th>
+                            <td>
+                                <p>Wartość produktów</p>
+                            </td>
+                            <td className='table-gap'>
+
+                            </td>
+                            <td>
+                                <p id="totalPrice">{total}{currency}</p>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        exampleList.map((item)=>(
-                            <ItemCard key={item.id} item={item} currency={currency} />
-                        ))
-                    }
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="btn-buy"><a href="shopping-cart"><b>Buy now</b></a></td>
-                        <td className="total-price-field"><p><b>{total} {currency}</b></p></td>
-                    </tr>
-                    </tbody>
-                </table>
+                        <tr>
+                            <td>
+                                <p>Koszt dostawy</p>
+                            </td>
+                            <td className='table-gap'>
+                                
+                            </td>
+                            <td>
+                                <p id='deliveryPrice'>{delivery}zł</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <hr className='middle-hr'></hr>
+                <div className='right-inner-shopping-cart'>
+                    <div className='center-flex'>
+                    <table>
+                        <tr>
+                            <td>
+                                <p>Suma</p>
+                            </td>
+                            <td className='table-gap'>
+                            </td>
+                        </tr>
+                    </table>
+                    <p id='sum-cost'>{total+parseFloat(delivery)}zł</p>
+                    </div>
+                    <div className='center-flex'>
+                        <button>Przejdź do kasy</button>
+                    </div>
+                </div>
+                <hr></hr>
             </div>
         </div>
+        <hr className='bottom-hr'/>
+        <div className='bottom-cart'>
+            <div className='bottom-left'>
+                <Link to="/">
+                    <p>&lt;&lt; Wróć do sklepu</p>
+                </Link>
+            </div>
+            <div className='bottom-right'>
+                <p className='right-align'>&copy; Kercz {currentYear()}</p>
+            </div>
+        </div>
+    </div>
     );
   }
   
